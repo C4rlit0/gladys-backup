db2ce27f6ff1f0d977d61d7714edd9d2ce4ea206
C4rlit0 <charly.maillefaud@gmail.com>
2017-08-13 14:54:54 +0200

    Fix bug with backup directory
    When use crontab command specified in readme.md the backup doesn't work fine because the database is backed up in /home/pi instead of /home/pi/gladys-backup
    
    Change the mysqldump command to force backup in gladys-backup folder

--- GdriveGladysBackup.sh
+++ GdriveGladysBackup.sh
@@ -2,7 +2,7 @@
 echo '// SAUVEGARDE //'
 echo 'Création de la sauvegarde de la base de données'
 var_BKP_FILE_NAME=gladys-$(date +%Y-%m-%d-%H-%M).sql
-mysqldump -uroot -proot gladys > "$var_BKP_FILE_NAME"
+mysqldump -uroot -proot gladys > /home/pi/gladys-backup/"$var_BKP_FILE_NAME"
 echo 'Sauvegarde créée'
 echo 'Upload vers Google Drive'
 cd /home/pi/gladys-backup
