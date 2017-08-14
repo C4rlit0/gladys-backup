922f076e11f5d3b880978b0049cfd3599e02e949
C4rlit0 <charly.maillefaud@gmail.com>
2017-08-15 01:12:45 +0200

    Update GdriveGladysInstall.sh

--- GdriveGladysInstall.sh
+++ GdriveGladysInstall.sh
@@ -15,4 +15,4 @@ sleep 2
 var_GLDS_FLD_ID=$(cat GLDS_FLD_ID.txt)
 ./gdrive mkdir -p "$var_GLDS_FLD_ID" GLDS_BDD_BKP | cut -d' ' -f2 > GLDS_BKP_FLD_ID.txt
 echo 'Arborescence de sauvegarde créée'
-echo 'Lancez votre premiere sauvegarde via ./GdriveGladysbackup.sh'
+echo 'Lancez votre premiere sauvegarde via ./GdriveGladysBackup.sh'
